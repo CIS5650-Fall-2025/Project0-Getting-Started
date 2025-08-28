065a7e0c2c67dfed6223a3fdc45767e82d0a4530
Xinghua Han <61567544+SirEnri2001@users.noreply.github.com>
2025-08-28 11:01:21 -0400

    Add Project 0: Henry Han

--- cuda-gl-check/src/main.cpp
+++ cuda-gl-check/src/main.cpp
@@ -11,7 +11,7 @@
  */
 int main(int argc, char* argv[]) {
     // TODO: Change this line to use your name!
-    m_yourName = "TODO: YOUR NAME HERE";
+    m_yourName = "Henry Han";
 
     if (init(argc, argv)) {
         mainLoop();
