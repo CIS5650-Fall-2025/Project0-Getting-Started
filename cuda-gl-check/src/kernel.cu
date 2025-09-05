95706a0a13b9828bc060cede6a75134eff84f4ee
Thomas Shaw <printer.83mph@gmail.com>
2025-09-05 18:20:15 -0400

    Format all source code with clang

--- cuda-gl-check/src/kernel.cu
+++ cuda-gl-check/src/kernel.cu
@@ -7,17 +7,17 @@
  * @copyright University of Pennsylvania
  */
 
+#include "kernel.h"
 #include <cstdio>
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
-#include "kernel.h"
 
 void checkCUDAError(const char *msg) {
-    cudaError_t err = cudaGetLastError();
-    if (cudaSuccess != err) {
-        fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
+  cudaError_t err = cudaGetLastError();
+  if (cudaSuccess != err) {
+    fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString(err));
+    exit(EXIT_FAILURE);
+  }
 }
 
 /**
@@ -37,72 +37,116 @@ void checkCUDAError(const char *msg) {
  * 12 -> Magenta        #ff0080     rgb(255,   0, 128)
  * *  -> Black          #000000     rgb(  0,   0,   0)
  */
-__host__ __device__ void versionToColor(uchar4* pixel, int version) {
-    switch(version) {
-        case 0:
-            pixel->x = 255; pixel->y = 255; pixel->z = 255; break;
-        case 1:
-            pixel->x = 255; pixel->y =   0; pixel->z =   0; break;
-        case 2:
-            pixel->x = 255; pixel->y = 128; pixel->z =   0; break;
-        case 3:
-            pixel->x = 255; pixel->y = 255; pixel->z =   0; break;
-        case 4:
-            pixel->x = 128; pixel->y = 255; pixel->z =   0; break;
-        case 5:
-            pixel->x =   0; pixel->y = 255; pixel->z =   0; break;
-        case 6:
-            pixel->x =   0; pixel->y = 255; pixel->z = 128; break;
-        case 7:
-            pixel->x =   0; pixel->y = 255; pixel->z = 255; break;
-        case 8:
-            pixel->x =   0; pixel->y = 128; pixel->z = 255; break;
-        case 9:
-            pixel->x =   0; pixel->y =   0; pixel->z = 255; break;
-        case 10:
-            pixel->x = 128; pixel->y =   0; pixel->z = 255; break;
-        case 11:
-            pixel->x = 255; pixel->y =   0; pixel->z = 255; break;
-        case 12:
-            pixel->x = 255; pixel->y =   0; pixel->z = 128; break;
-        default:
-            pixel->x =   0; pixel->y =   0; pixel->z =   0;
-    }
+__host__ __device__ void versionToColor(uchar4 *pixel, int version) {
+  switch (version) {
+  case 0:
+    pixel->x = 255;
+    pixel->y = 255;
+    pixel->z = 255;
+    break;
+  case 1:
+    pixel->x = 255;
+    pixel->y = 0;
+    pixel->z = 0;
+    break;
+  case 2:
+    pixel->x = 255;
+    pixel->y = 128;
+    pixel->z = 0;
+    break;
+  case 3:
+    pixel->x = 255;
+    pixel->y = 255;
+    pixel->z = 0;
+    break;
+  case 4:
+    pixel->x = 128;
+    pixel->y = 255;
+    pixel->z = 0;
+    break;
+  case 5:
+    pixel->x = 0;
+    pixel->y = 255;
+    pixel->z = 0;
+    break;
+  case 6:
+    pixel->x = 0;
+    pixel->y = 255;
+    pixel->z = 128;
+    break;
+  case 7:
+    pixel->x = 0;
+    pixel->y = 255;
+    pixel->z = 255;
+    break;
+  case 8:
+    pixel->x = 0;
+    pixel->y = 128;
+    pixel->z = 255;
+    break;
+  case 9:
+    pixel->x = 0;
+    pixel->y = 0;
+    pixel->z = 255;
+    break;
+  case 10:
+    pixel->x = 128;
+    pixel->y = 0;
+    pixel->z = 255;
+    break;
+  case 11:
+    pixel->x = 255;
+    pixel->y = 0;
+    pixel->z = 255;
+    break;
+  case 12:
+    pixel->x = 255;
+    pixel->y = 0;
+    pixel->z = 128;
+    break;
+  default:
+    pixel->x = 0;
+    pixel->y = 0;
+    pixel->z = 0;
+  }
 }
 
 // Kernel that writes the image to the OpenGL PBO directly.
-__global__ void createVersionVisualization(uchar4* PBOpos, int width, int height, int major, int minor) {
-    int x = (blockIdx.x * blockDim.x) + threadIdx.x;
-    int y = (blockIdx.y * blockDim.y) + threadIdx.y;
-    int index = x + (y * width);
+__global__ void createVersionVisualization(uchar4 *PBOpos, int width,
+                                           int height, int major, int minor) {
+  int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+  int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+  int index = x + (y * width);
 
-    if (x <= width && y <= height) {
-        // Each thread writes one pixel location in the texture (textel)
-        PBOpos[index].w = 0;
-        PBOpos[index].x = 0;
-        PBOpos[index].y = 0;
-        PBOpos[index].z = 0;
+  if (x <= width && y <= height) {
+    // Each thread writes one pixel location in the texture (textel)
+    PBOpos[index].w = 0;
+    PBOpos[index].x = 0;
+    PBOpos[index].y = 0;
+    PBOpos[index].z = 0;
 
-        int ver = y < height / 2 ? major : minor;
-        versionToColor(&PBOpos[index], ver);
-    }
+    int ver = y < height / 2 ? major : minor;
+    versionToColor(&PBOpos[index], ver);
+  }
 }
 
 // Wrapper for the __global__ call that sets up the kernel calls
-void kernelVersionVis(uchar4* PBOpos, int width, int height, int major, int minor) {
-    // set up crucial magic
-    unsigned int blockSize = 16;
-    dim3 threadsPerBlock(blockSize, blockSize);
+void kernelVersionVis(uchar4 *PBOpos, int width, int height, int major,
+                      int minor) {
+  // set up crucial magic
+  unsigned int blockSize = 16;
+  dim3 threadsPerBlock(blockSize, blockSize);
 
-    unsigned int blocksX = (width + blockSize - 1) / blockSize;
-    unsigned int blocksY = (height + blockSize - 1) / blockSize;
-    dim3 fullBlocksPerGrid(blocksX, blocksY);
+  unsigned int blocksX = (width + blockSize - 1) / blockSize;
+  unsigned int blocksY = (height + blockSize - 1) / blockSize;
+  dim3 fullBlocksPerGrid(blocksX, blocksY);
 
-    //kernel launches
-    createVersionVisualization<<<fullBlocksPerGrid, threadsPerBlock>>>(PBOpos, width, height, major, minor);
+  // kernel launches
+  createVersionVisualization<<<fullBlocksPerGrid, threadsPerBlock>>>(
+      PBOpos, width, height, major, minor);
 
-    // make certain the kernel has completed
-    cudaDeviceSynchronize();
+  // make certain the kernel has completed
+  cudaDeviceSynchronize();
 
-    checkCUDAError("Kernel failed!");
+  checkCUDAError("Kernel failed!");
 }
